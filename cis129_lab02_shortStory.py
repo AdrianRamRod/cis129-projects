a5607018fb6dda5b4faa317d7d3783e843eff315
AdrianRamRod <157067779+AdrianRamRod@users.noreply.github.com>
2024-02-07 20:16:19 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -2,3 +2,6 @@ print('The mariachi sang his heart out.'
 print('He sang with the guitar on his side')
 print('The beautiful women in the crowd begged for his attention')
 print('Off to the next town he goes striving for his success.')
+# Author: Adrian Ramirez
+# My First Python lab in my CIS129 Course
+#Print a simple short story
