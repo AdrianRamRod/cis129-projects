67d6b34196c95555220004d213921f93ba06ea8a
AdrianRamRod <157067779+AdrianRamRod@users.noreply.github.com>
2024-02-07 20:15:23 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,4 @@
-print('Hello, World!')
+print('The mariachi sang his heart out.'
+print('He sang with the guitar on his side')
+print('The beautiful women in the crowd begged for his attention')
+print('Off to the next town he goes striving for his success.')
